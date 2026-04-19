83faa6912f6e3fed5e68004e36f4a4a8ce2923b0
SilverRuler <SilverRuler@example.com>
2026-04-19 18:25:19 +0900

    Provide placeholders for all Pusher config to fix build error

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -2,20 +2,20 @@ import PusherServer from 'pusher'
 import PusherClient from 'pusher-js'
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || 'mt1', // 기본값 제공
+  appId: process.env.PUSHER_APP_ID || '',
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY || '',
+  secret: process.env.PUSHER_SECRET || '',
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || 'mt1',
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY || '',
   {
     channelAuthorization: {
       endpoint: '/api/pusher/auth',
       transport: 'ajax',
     },
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || 'mt1', // 기본값 제공
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || 'mt1',
   }
 );
